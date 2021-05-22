d5e3546ce69571222a2dc3751164292f1940cb83
subhadeep <subhadeepc19@gmail.com>
2021-05-22 18:38:27 +0530

    commit a react app ad publish it to github pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
+import React , { useRef } from 'react'
+// import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs"
+import * as facemsh from "@tensorflow-models/facemesh"
+import Webcam from "react-webcam"
+import { drawMesh } from './utilities';
 
 function App() {
+
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  //Load facemesh
+  const runFacemesh = async () => {
+    const net = await facemsh.load({
+      inputResolution: {width: 604, height: 480}, scale: 0.8
+    })
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video)
+      // console.log(face)
+
+      const ctx = canvasRef.current.getContext("2d");
+      drawMesh(face, ctx)
+    }
+  }
+  runFacemesh();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className = "App-header">
+      <Webcam ref={webcamRef} style = {
+        {
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left:0,
+          right: 0,
+          textALign: "center",
+          zIndex: 9,
+          width:640,
+          height: 480
+        }  
+      } />
+      <canvas ref= {canvasRef} style= {{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left:0,
+          right: 0,
+          textALign: "center",
+          zIndex: 9,
+          width:640,
+          height: 480
+      }}></canvas>
       </header>
     </div>
   );
